640845648c423e6cb3bbd3ed324969968ed441d0
João Gabriel <main.joaogabrielsg@gmail.com>
2022-06-22 22:03:59 -0300

    feat: add project of the day 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Event } from "./pages/Event";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { client } from "./lib/apollo";
+import { Router } from "./Router";
 
 function App() {
-  return <Event />;
+  return (
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <Router />
+      </ApolloProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { format, isPast } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -18,10 +19,10 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
   );
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -40,6 +41,6 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
